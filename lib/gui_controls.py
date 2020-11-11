f9be6b6027a735f3ecf31bcace4d7306c5b80462
liuyu <674197141@qq.com>
2020-11-11 11:49:12 +0800

    鼠标控制放到lib里

---
+++ lib/gui_controls.py
@@ -0,0 +1,27 @@
+import pyautogui
+
+# 路径，对比度
+def get_screen_box(path,contrast_ratio = 0.9):
+    return pyautogui.locateOnScreen(path,confidence=contrast_ratio)
+
+# 右键单击,路径中不能有中文
+def right_click(path):
+    coords = get_screen_box(path)
+    if coords:
+        #获取定位到的图中间点坐标
+        x,y=pyautogui.center(coords)
+        #右击该坐标点
+        pyautogui.rightClick(x,y)
+        return True
+    return False
+
+# 左键单击,路径中不能有中文
+def left_click(path):
+    coords = get_screen_box(path)
+    if coords:
+        #获取定位到的图中间点坐标
+        x,y=pyautogui.center(coords)
+        #右击该坐标点
+        pyautogui.leftClick(x,y)
+        return True
+    return False
