79109bf17f25ef51c73b6757d947657f79524978
liuyu <674197141@qq.com>
2020-11-27 11:15:13 +0800

    采集模块提交

--- game_robot/jiuyin/script/CaiJi.py
+++ game_robot/jiuyin/script/CaiJi.py
@@ -2,14 +2,15 @@ from lib.BaseModule import BaseModule
 from lib.gui_controls import Controls
 import pyautogui
 from loguru import logger
+import win32gui, win32con, win32api
 
-# 内功
-class NeiGong(BaseModule):
+# 采集
+class CaiJi(BaseModule):
 
     is_act = True
 
     def __init__(self):
-        logger.info("初始化内功模块")
+        logger.info("初始化采集模块")
 
     def update_hwnd(self,hwnd):
         up = Controls.locate("image\\ng_jxxl.png", hwnd)
