b026e241079208fe3e49c40af138074329e43255
liuyu <674197141@qq.com>
2021-01-27 14:15:31 +0800

    增加堆栈异常报错

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import lib.global_data as gbd
 from loguru import logger
 from config import init_config
 from model.main_windows import show_login
-
+import traceback
 
 def main():
-    succ = init_config()  #初始化配置
-    if succ is None:
-        return
-    logger.info("设置九阴窗口")
-    import game_robot
-    gbd.Exit = False
-    show_login()
-
+    try:
+        succ = init_config()  #初始化配置
+        if succ is None:
+            return
+        logger.info("设置九阴窗口")
+        import game_robot
+        gbd.Exit = False
+        show_login()
+    except Exception as e:
+        logger.error(str(e))
+        msg = traceback.format_exc()
+        logger.error(msg)
 
 if __name__ == "__main__":
     main()
